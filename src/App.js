45daeaf694b2f1cfeda7d7933c0767515262a78d
Jack <jackdyke@hotmail.com>
2020-12-28 14:58:37 +0000

    added navbar, routing and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Component } from 'react';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Read } from './components/Read';
+import { Create } from './components/Create';
+import { Content } from './components/Content';
+import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      //our navbar borrowed from react bootstrap
+      //the router that is being used to define the paths for each component
+
+      <Router>
+        <div className="App">
+
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Articles</Nav.Link>
+              <Nav.Link href="/create">Editor</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar>
+
+          <br />
+
+          <Switch>
+            <Route path='/' component={Content} exact />
+            <Route path='/create' component={Create} exact />
+            <Route path='/read' component={Read} exact />
+          </Switch>
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Read.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export class Read extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>This is the Read component.</h1>
+            </div>
+        );
+    }
+}
