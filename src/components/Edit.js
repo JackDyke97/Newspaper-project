b3f37f3436c9b3b9140ed4c5819d5f6277e6072e
Jack <jackdyke@hotmail.com>
2021-01-05 03:34:23 +0000

    added edit

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import axios from'axios';
 
+//create component used to display in the app 
+
 export class Edit extends React.Component {
-    constructor(){
+
+    constructor() {
         super();
-        //binding the onchange functions
+//binding the onChange functions 
         this.onSubmit = this.onSubmit.bind(this);
         this.onChangeTitle = this.onChangeTitle.bind(this);
         this.onChangeAuthor = this.onChangeAuthor.bind(this);
         this.onChangeText = this.onChangeText.bind(this);
         this.onChangeImage = this.onChangeImage.bind(this);
-
-        //set the inputs for the forms
+//Set the actual inputs for the forms
         this.state = {
             Title: '',
             Author: '',
@@ -19,27 +21,26 @@ export class Edit extends React.Component {
             Image: ''
         }
     }
-
+//using a get component method to call the function in our server which is going to grab the id and allow us to edit it
     componentDidMount(){
         console.log(this.props.match.params.id);
-//getting our app.get function from our server.js to allow us to get the variables and id for our data
+
         axios.get('http://localhost:4000/api/articles/' +this.props.match.params.id)
-        .then(response => {
+        .then(response=>{
             this.setState({
                 _id:response.data._id,
                 Title:response.data.title,
                 Author:response.data.author,
                 Text:response.data.text,
                 Image:response.data.image
-
-            });
+            })
         })
         .catch((error)=>{
-                console.log(error);
-        })
+            console.log(error);
+         })
     }
 
-    //The onchange functions for title, year and poster. These are used to target the values we enter
+//The onchange functions for title, year and poster. These are used to target the values we enter
     onChangeTitle(e) {
         this.setState({
             Title: e.target.value
@@ -67,53 +68,58 @@ export class Edit extends React.Component {
 
     onSubmit(e) {
         e.preventDefault();
-        alert("Article: " + this.state.Title + " " + this.state.Author + " " + this.state.Text + " " + this.state.Image);
+        alert("Article: " + this.state.Title + " " + this.state.Author + " " + this.state.Text+" "+ this.state.Image);
 
         const editArticle = {
             title:this.state.Title,
             author:this.state.Author,
-            text:this.state.Text,
-            image:this.state.Image,
+            text: this.state.Text,
+            image: this.state.Image,
             _id: this.state._id
-        };
+        }
 
-        axios.put('http:localhost:4000/api/articles/' +this.state._id, editArticle)
-        .then(res => {
+        axios.put('http://localhost:4000/api/articles/'+this.state._id, editArticle, )
+        .then(res =>{
             console.log(res.data)
         })
-        .catch((err => {
+        .catch((err=>{
             console.log(err);
         }))
-}
-        render() {
-            return(
-                <div className="App">
-                <form onSubmit={this.onSubmit}></form>
-                {/* first div tag is for input control
-                    The other tags are for setting the title,author,image and text respectively  
-                    The label is used to show the text on our website
-                    the  input allows us to define what type we want and to tyle it using className
-                    and also to define the value that is called*/}
-                <div className="form-group">
-                    <label>Edit Article Title</label>
-                    <input type='text' className='form-control' value={this.state.Title} onChange={this.onChangeTitle}></input>
-                </div>
-                <div className="form-group">
-                    <label>Edit Article Author</label>
-                    <input type='text' className='form-control' value={this.state.Author} onChange={this.onChangeAuthor}></input>
-                </div>
-                <div className="form-group">
-                    <label>Edit Article Text</label>
-                    <input type='text' className='form-control' value={this.state.Text} onChange={this.onChangeText}></input>
-                </div>
-                <div className="form-group">
-                    <label>Edit Article Image</label>
-                    <textarea type='text' className='form-control' value={this.state.Image} onChange={this.onChangeImage}></textarea>
-                </div>
-                <div className="form-group">
-                    <input type='submit' value='Edit Article' className='btn btn-primary'></input>
-                </div>
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Edit Article Title: </label>
+                        <input type='text' className='form-control' value={this.state.Title}
+                            onChange={this.onChangeTitle}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Edit Article Author:</label>
+                        <input type='text' className='form-control' value={this.state.Author}
+                        onChange={this.onChangeAuthor}></input>
+                        </div>
+                        <div className="form-group">
+                        <label>Edit Article Text:</label>
+                        <input type='text' className='form-control' value={this.state.Text}
+                        onChange={this.onChangeText}></input>
+                        </div>
+                    <div className='form-group'>
+                        <label>Article Picture:</label>
+                        <textarea type='text' className='form-control' value={this.state.Image}
+                        onChange={this.onChangeImage}>
+
+                        </textarea>
+                    </div>
+                    <div className="form-group">
+                        <input type='submit' className='btn btn-primary' value='Edit Article'>
+                        </input>
+                    </div>
+                </form>
+
             </div>
-            );
-        }
+        );
+    }
 }
